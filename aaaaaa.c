f970fd972f98008816b67db1d6e2344e56999e6d
Nadezhda Naumova <nnaumova@devexperts.com>
2020-11-01 20:58:45 +0300

    Iteration

--- aaaaaa.c
+++ aaaaaa.c
@@ -7,11 +7,10 @@
 #include <sys/uio.h>
 #include <unistd.h>
 #include <stdint.h>
-#include <cmath.h>
 
 #define MALLOC_SIZE 84000000
 #define FILL_THREADS 127
-#define BLOCK_SIZE 33
+#define BLOCK_SIZE 100 // was 33
 #define FILE_SIZE 185000000
 #define FILE_COUNT MALLOC_SIZE / FILE_SIZE + (MALLOC_SIZE % FILE_SIZE == 0 ? 0 : 1)
 
@@ -33,6 +32,10 @@ void fillMemoryRegion(int, void*, size_t);
 void* fillMemoryRegionProxy(void*);
 void writeRegionToFile(void*);
 void writeFile(int, void*, size_t);
+int countNodes(struct LinkedListNode *);
+int pickRandomBlock(struct LinkedListNode *);
+struct LinkedListNode * selectNodeBy(struct LinkedListNode *, int);
+void removeNode(struct LinkedListNode *, int);
 
 int main() {
     while (1) {
@@ -95,16 +98,18 @@ void writeFile(int fd, void* address, size_t size) {
     node.size = count;
     node.startBlock = 0;
     node.next = NULL;
+    int blocksWritten = 0;
 
     while(1) {
-        int i = pickRandomBlock(node, count);
+        int i = pickRandomBlock(&node);
         if (i == -1)
             break;
         lseek(fd, i * BLOCK_SIZE, SEEK_SET);
         if (i == count - 1)
             write(fd, (void*)((uint8_t *)address + i * count), size - BLOCK_SIZE * (count - 1));
         else write(fd, (void*)((uint8_t *)address + i * count), BLOCK_SIZE);
-        printf("Writing %d blocks of %d\r", i, count);
+        blocksWritten++;
+        printf("Writing %d blocks of %d\r", blocksWritten, count);
         fflush(stdout);
     }
 }
@@ -113,13 +118,83 @@ void writeFile(int fd, void* address, size_t size) {
  * и возвращает номер этого блока
  * Этот блок удаляется из списка промежутков и, если свободных блоков не осталось, он возвращает -1
 */
-int pickRandomBlock(struct LinkedListNode * node, int count) {
-    int randomNumber = rand() % (count + 1);
-//    if (randomNumber)
+int pickRandomBlock(struct LinkedListNode * node) {
+    int partitionCount = countNodes(node);
+    int selectedNodeIdx = rand() % partitionCount;
+    struct LinkedListNode * selectedNode = selectNodeBy(node, selectedNodeIdx);
+    if (partitionCount == 1 && selectedNode->size == 0)
+        return -1;
+    int randomBlockNumber = rand() % selectedNode->size;
+    int result = selectedNode->startBlock + randomBlockNumber;
+    if (randomBlockNumber == selectedNode->size - 1) {
+        selectedNode->size --;
+        if (selectedNode->size == 0)
+            removeNode(node, selectedNodeIdx);
+    } else if (randomBlockNumber == 0) {
+        selectedNode->startBlock ++;
+        selectedNode->size --;
+    } else {
+        int size = selectedNode->size - 1;
+        selectedNode->size = randomBlockNumber;
+        struct LinkedListNode * newNode = (struct LinkedListNode *)malloc(sizeof(struct LinkedListNode));
+        newNode->startBlock = randomBlockNumber + 1;
+        newNode->size = size - selectedNode->size;
+        struct LinkedListNode * prevNextNode = node->next;
+        node->next = newNode;
+        newNode->next = prevNextNode;
+    }
+//    struct LinkedListNode* current = node;
+//    int _count = 0;
+//    while (current != NULL) {
+//        if (current->size == 0) {
+//            printf("AAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+//            printf("start %d\n", current->startBlock);
+//            printf("count %d\n", _count);
+//            exit(1);
+//        }
+//        current = current->next;
+//        _count++;
+//    }
+    return result;
+}
 
-    node->size += 1;
+void removeNode(struct LinkedListNode * node, int index) {
+    if (index == 0) {
+        if (node->next == NULL) {
+            node->next = 0;
+            node->size = 0;
+            node->startBlock = 0;
+            return;
+        }
 
+        node->startBlock = node->next->startBlock;
+        node->size = node->next->size;
+        struct LinkedListNode * toBeDeleted = node->next;
+        node->next = node->next->next;
+        free((void *)toBeDeleted);
+    } else {
+        struct LinkedListNode * prevDeletedElement = selectNodeBy(node, index - 1);
+        struct LinkedListNode * toBeDeleted = prevDeletedElement->next;
+        prevDeletedElement->next = prevDeletedElement->next->next;
+        free((void *)toBeDeleted);
+    }
+}
 
+struct LinkedListNode * selectNodeBy(struct LinkedListNode * node, int position) {
+    for (int i = 0; i < position; i ++)
+        node = node->next;
+    return node;
+}
+
+int countNodes(struct LinkedListNode * node) {
+    int count = 1;
+    if (node == NULL)
+        return 0;
+    while (node->next != NULL) {
+        count ++;
+        node = node->next;
+    }
+    return count;
 }
 
 void* fillMemoryRegionProxy(void* argsPointer) {
